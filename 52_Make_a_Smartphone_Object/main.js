09f89af99a5920343abb334b99a1e178b4a6e165
Dream_4r <145902745+Unicorn4rich@users.noreply.github.com>
2024-05-30 23:12:02 +0500

    first

---
+++ 52_Make_a_Smartphone_Object/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Q52 - Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand,
+// model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+// ab ham object bnaenge or isme ye opar wali data type assigne kar denge... 
+let smartphone = {
+    brand: "Redmi",
+    model: "Note 12",
+    features: {
+        storage: "128 GB",
+        screen_size: 6.7,
+        battery: "5000 MH"
+    }
+};
+console.log(smartphone);
---
+++ 53_Pulling_Apart_a_Nested_Object/main.ts
@@ -0,0 +1,21 @@
+// Q53 - Pulling Apart a Nested Object: Imagine you have a list inside another list that shows what a
+// computer programmer knows, like coding languages, tools, and software frameworks. Find a way to get
+// three specific skills from this list and show them.
+
+
+let programmer_skills = {
+
+    languages: ["HTML", "CSS", "TypeScript", "Python"],
+
+    frameworks: ["Ruby on Rails", "Angular", "Spring"],
+
+    tools: ["Git", "Subversion", "Mercurial"],
+
+  };
+  
+
+  let { languages, frameworks, tools } = programmer_skills;
+  
+
+  console.log(`Language: ${languages[0]}, Framework: ${frameworks[0]}, Tool: ${tools[0]}`);
+
